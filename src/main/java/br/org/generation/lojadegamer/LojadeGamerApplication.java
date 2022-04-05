ebab3b70626d498b8011b1ed5b0e46d65b024de3
Alessandra Silva <alessandra.silva070996@gmail.com>
2022-04-05 09:29:18 -0300

    lojadegamer

---
+++ src/main/java/br/org/generation/lojadegamer/LojadeGamerApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.lojadegamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojadeGamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojadeGamerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/org/generation/lojadegamer/repository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package br.org.generation.lojadegamer.repository;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.org.generation.lojadegamer.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+	public List<Categoria> findAllByTipoContainingIgnoreCase(String tipo);
+
+}
